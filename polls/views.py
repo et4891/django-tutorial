fc64448d8bc4f62d40bafc6b75ec895fb447b19b
ET-Gate <et1103@hotmail.com>
2016-04-02 02:56:27 -0700

    Part 3, done 'Raising a 404 error' continue 'Use the template system'

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,6 @@
 from django.http import HttpResponse
-from django.shortcuts import Http404
 
-# This option uses render to render the view instead of using template
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 from .models import Question
 
@@ -16,10 +14,7 @@ def index(request):
 
 
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
+    question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question': question})
 
 
