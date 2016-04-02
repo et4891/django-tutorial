3720e7de34b35227714b00f1c176e1c5ed31ce92
ET-Gate <et1103@hotmail.com>
2016-04-02 02:50:35 -0700

    Using Http404, will try using shortcut:get_object_or_404() in next commit

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,5 @@
 from django.http import HttpResponse
-
-# Uses loader to load template which will require the template variable below to be active
-# There is another option which uses a shortcut called render
-from django.template import loader
+from django.shortcuts import Http404
 
 # This option uses render to render the view instead of using template
 from django.shortcuts import render
@@ -12,17 +9,18 @@ from .models import Question
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    # template = loader.get_template('polls/index.html') (This uses template)
     context = {
         'latest_question_list': latest_question_list,
     }
-    # output = ', '.join([q.question_text for q in latest_question_list]) (This is hard coded which is not a good idea)
-    # return HttpResponse(template.render(context, request)) (This uses template)
     return render(request, 'polls/index.html', context)
 
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
 
 
 def results(request, question_id):
